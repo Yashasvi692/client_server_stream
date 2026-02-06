fb4740e32de00afd0e3d9f98afc198bf5717ec38
Yashasvi692 <154086545+Yashasvi692@users.noreply.github.com>
2026-02-06 14:48:53 +0530

    huggingface

--- client_server_stream/server/plugins/llm_demo.py
+++ client_server_stream/server/plugins/llm_demo.py
@@ -1,16 +1,45 @@
 import asyncio
 from .base import StreamPlugin
+import asyncio
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
+from threading import Thread
+import torch
 
 
 class LLMDemoPlugin(StreamPlugin):
     name = "llm_demo"
 
-    async def stream(self, prompt: str):
-        print("LLM STREAM CALLED WITH:", prompt)
-        response = f"Simulated LLM response to: {prompt}"
+    def __init__(self):
+        print("Loading HF model...")
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+        )
+        self.model = AutoModelForCausalLM.from_pretrained(
+            "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+            device_map="auto",
+            torch_dtype=torch.float32
+        )
 
-        # simulate token streaming
-        for word in response.split():
-            print("LLM CHUNK:", word)
-            await asyncio.sleep(0.3)
-            yield word + " "
+    async def stream(self, prompt: str):
+        print("HF STREAM:", prompt)
+
+        streamer = TextIteratorStreamer(
+            self.tokenizer,
+            skip_prompt=True
+        )
+
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+
+        thread = Thread(
+            target=self.model.generate,
+            kwargs=dict(
+                **inputs,
+                streamer=streamer,
+                max_new_tokens=100
+            )
+        )
+        thread.start()
+
+        for token in streamer:
+            await asyncio.sleep(0)
+            yield token
