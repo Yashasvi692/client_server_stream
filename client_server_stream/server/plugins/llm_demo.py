bd3a1f6fa42237d4252f4fedd38683f0b85b7f04
Yashasvi692 <154086545+Yashasvi692@users.noreply.github.com>
2026-02-06 15:56:14 +0530

    tokens update

--- client_server_stream/server/plugins/llm_demo.py
+++ client_server_stream/server/plugins/llm_demo.py
@@ -1,45 +1,21 @@
-import asyncio
+import httpx
 from .base import StreamPlugin
-import asyncio
-from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
-from threading import Thread
-import torch
 
 
 class LLMDemoPlugin(StreamPlugin):
     name = "llm_demo"
 
-    def __init__(self):
-        print("Loading HF model...")
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-        )
-        self.model = AutoModelForCausalLM.from_pretrained(
-            "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-            device_map="auto",
-            torch_dtype=torch.float32
-        )
-
     async def stream(self, prompt: str):
-        print("HF STREAM:", prompt)
+        print("CALLING HF SERVICE:", prompt)
 
-        streamer = TextIteratorStreamer(
-            self.tokenizer,
-            skip_prompt=True
-        )
+        async with httpx.AsyncClient(timeout=None) as client:
+            async with client.stream(
+                "POST",
+                "https://unirritable-onomatopoetically-donna.ngrok-free.dev/generate",
+                json={"prompt": prompt},
+            ) as response:
 
-        inputs = self.tokenizer(prompt, return_tensors="pt")
-
-        thread = Thread(
-            target=self.model.generate,
-            kwargs=dict(
-                **inputs,
-                streamer=streamer,
-                max_new_tokens=100
-            )
-        )
-        thread.start()
-
-        for token in streamer:
-            await asyncio.sleep(0)
-            yield token
+                async for chunk in response.aiter_text():
+                    if chunk.strip():
+                        print("TOKEN:", chunk)
+                        yield chunk
